24fb2694aa3be1780853cce6b9b3655b2f42e469
DangHaThe <dangars151@gmail.com>
2022-08-17 14:55:51 +0700

    feat: container with most water and trapping rain water

---
+++ two-pointers/container-with-most-water.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(maxArea(height))
+}
+
+func maxArea(height []int) int {
+	maxArea := 0
+	left, right := 0, len(height)-1
+
+	for left < right {
+		maxArea = max(maxArea, min(height[left], height[right])*(right-left))
+		if height[left] > height[right] {
+			left += 1
+		} else {
+			right -= 1
+		}
+	}
+
+	return maxArea
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
